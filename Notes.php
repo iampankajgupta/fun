5e6895be8761e11c718696ec2e82c6b09a865f6c
iampankajgupta <itsmeguptapankaj@gmail.com>
2018-11-04 20:42:45 +0530

    Notes.html

---
+++ Notes.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>
+Notes
+</title>
+</head>
+<body BGCOLOR = 'GRAY'> 
+<form align = "center" method = "post" action = "welcome.php">
+username:<input type = "text" name = "username" required ><br><br>
+email:<input type = "text" name =  "email" required><br><br>
+password:<input type = "text" name = "password"  required><br><br>
+<input type = "submit">
+<center><a href = "signin.php" >Sign in </a></center>
+</form>
+</body>
+</html> 
