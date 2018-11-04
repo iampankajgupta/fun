b711df67121456f438b5377964f55e6d87079b71
iampankajgupta <itsmeguptapankaj@gmail.com>
2018-11-04 20:43:35 +0530

    welcome page

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>
+		Welcome
+	</title>
+	</head>
+	<body align = "center" BGCOLOR = "GRAY">
+		<h1> Hello </h1>
+		<input type = "text" >
+		<button type = "button ">ADD</button>
+	</body>
+
+</html>
