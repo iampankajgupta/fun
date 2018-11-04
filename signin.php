256792901bfe02d7b51962588418d1d3f62a7af9
iampankajgupta <itsmeguptapankaj@gmail.com>
2018-11-04 20:44:27 +0530

    signin

---
+++ signin.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>
+		Sign in
+	</title>
+</head>
+<body align = "center" BGCOLOR = "GRAY">
+	<form  method = "post" action = "welcome.php">
+		Email:<input type = "text" name = "email" required><br><br>
+		Password:<input type = "text"  name = "password" required><br><br>
+		<button type = "button "> Submit </button><br>
+		<center><a href = "Notes.html" color = "black"> Sign up</a></center>
+	</form>
+	</body>
+</html>
